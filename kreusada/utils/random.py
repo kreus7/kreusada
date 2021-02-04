9f5b8b97122ecbbf6688726b8a42c1743ed2836a
Kreusada <kreusadaprojects@gmail.com>
2021-02-04 18:42:34 +0000

    Bump 1.1.17

--- kreusada/utils/random.py
+++ kreusada/utils/random.py
@@ -3,7 +3,7 @@ from random import choice as pick
 import pathlib
 
 def fruit(plural: bool):
-    with open("utils/datalists/consumables.yaml") as file:
+    with open(pathlib.Path(__file__).resolve(strict=True).parent / "datalists" / "consumables.yaml") as file:
         fruit = load(file)
 
         for item, doc in fruit.items():
