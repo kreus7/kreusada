d14066c41b157efc7e33213625b86c9e80c2c0a5
Kreusada <kreusadaprojects@gmail.com>
2021-02-04 20:56:11 +0000

    [1.1.23]

--- kreusada/utils/random.py
+++ kreusada/utils/random.py
@@ -5,6 +5,8 @@ import pathlib
 def fruit(plural: bool):
     with open(pathlib.Path(__file__).resolve(strict=True).parent / "datalists" / "consumables.yaml") as file:
         fruit = load(file)
-
         for item, doc in fruit.items():
-            return pick(doc['fruits']['singular']) if not plural else pick(doc['fruits']['plural'])
+            if not plural:
+                return pick(doc['fruits']['singular'])
+            else:
+                return pick(doc['fruits']['plural'])
