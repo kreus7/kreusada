d8ebc85166a187c4055cb9d0e414d86033eea0ae
Kreusada <kreusadaprojects@gmail.com>
2021-02-04 18:29:06 +0000

    1.1.12: More testing

--- kreusada/__init__.py
+++ kreusada/__init__.py
@@ -1,12 +1,11 @@
-from .tests import *
-from . import (
-    codeblocks,
-    chat_formatting,
-    embed,
-    random,
-    urls,
-    tags,
-)
+#from .. import (
+#    codeblocks,
+#    chat_formatting,
+#    embed,
+#    random,
+#    urls,
+#    tags,
+#)
 
 __author__ = tags.__author__
 __version__ = tags.__version__
