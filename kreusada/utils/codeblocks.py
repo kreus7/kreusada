032f641f7618fa7203ff179b6b0112cff3d9b08c
Kreusada <kreusadaprojects@gmail.com>
2021-02-07 19:16:44 +0000

    Merge all utils to one file. Version bump: 1.2.1.dev

--- kreusada/utils/codeblocks.py
+++
@@ -1,5 +0,0 @@
-def box(text: str, lang: str):
-    return "```{}\n{}\n```".format(lang, text)
-
-def backtick(text: str):
-    return "`{}`".format(text)
--- kreusada/utils/embed.py
+++
@@ -1,4 +0,0 @@
-from random import randint as r
-
-def random_colour():
-    return "%06x" % r(0, 0xFFFFFF)
