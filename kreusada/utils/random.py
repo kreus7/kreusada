fb4e410e6a65799cfa91fa7e5b723c1c972df5f2
Kreusada <kreusadaprojects@gmail.com>
2021-02-07 18:27:31 +0000

    A few commits

--- kreusada/utils/random.py
+++ kreusada/utils/random.py
@@ -1,12 +1,24 @@
-from yaml import full_load as load
-from random import choice as pick
+import yaml
+import random
 import pathlib
 
-def fruit(plural: bool):
-    with open(pathlib.Path(__file__).resolve(strict=True).parent / "datalists" / "consumables.yaml") as file:
-        fruit = load(file)
-        for k in fruit.keys():
-            if not plural:
-                return pick(k['singular'])
-            else:
-                return pick(k['plural'])
+def boolean():
+    """
+    Returns a random boolean.
+    """
+    return random.getrandbits(1)
+
+def percentage():
+    """
+    Returns a percentage from 1 to 100.
+    """
+    return f"{random.randint(1, 100)}%"
+
+#def fruit(plural: bool):
+#    with open(pathlib.Path(__file__).resolve(strict=True).parent / "datalists" / "consumables.yaml") as file:
+#        fruit = yaml.full_load(file)
+#        for k in fruit.keys():
+#            if not plural:
+#                return random.choice(k['singular'])
+#            else:
+#                return random.choice(k['plural'])
