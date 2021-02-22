31b7dbc2c4ed26547b0dcade52db9d2156aba062
kreusada <67752638+kreusada@users.noreply.github.com>
2021-02-22 18:34:02 +0000

    Import all

--- kreusada/__init__.py
+++ kreusada/__init__.py
@@ -1,3 +1,53 @@
-from .tags import __version__
+from .tags import (
+  __version__,
+  __author__,
+  __contributors,
+)
+
+from .box import (
+  full,
+  small,
+)
+
+from chat_formatting import (
+  bold,
+  italic,
+  strike,
+  spoiler,
+  quote,
+  snake,
+  reverse,
+)
+
+from .embed import (
+  randcolor,
+  randcolour,
+)
+
+from internals import (
+  platform,
+  ram,
+  os,
+  get_module,
+)
+
+from .numerics import (
+  isodd,
+  iseven,
+)
+
+from .random import (
+  boolean,
+  percentage,
+)
+
+from .urls import (
+  gist,
+  github,
+  lmgtfy,
+  google,
+)
 
 __version__ = __version__
+__author__ = __author__
+__contributors__ = __contributors__
